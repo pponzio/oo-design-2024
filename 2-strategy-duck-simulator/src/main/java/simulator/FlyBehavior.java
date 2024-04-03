db0d47dbec71dec1d2351cef8bc96a3c6b54d62f
Pablo Ponzio <pponzio@dc.exa.unrc.edu.ar>
2024-04-03 14:52:31 -0300

    Examples for the strategy design pattern

---
+++ 2-strategy-duck-simulator/src/main/java/simulator/FlyBehavior.java
@@ -0,0 +1,5 @@
+package simulator;
+
+public interface FlyBehavior {
+	public void fly();
+}
---
+++ 2-strategy-duck-simulator/src/main/java/simulator/Quack.java
@@ -0,0 +1,7 @@
+package simulator;
+
+public class Quack implements QuackBehavior {
+	public void quack() {
+		System.out.println("Quack");
+	}
+}
